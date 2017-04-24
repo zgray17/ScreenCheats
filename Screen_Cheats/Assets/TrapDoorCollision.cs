c5de69f08c702847408d3288cb0993d62fb86274
zgray17 <zgray17@jcu.edu>
2017-04-24 08:37:41 -0400

    test build one

---
+++ Screen_Cheats/Assets/TrapDoorCollision.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class TrapDoorCollision : MonoBehaviour {
+
+	private GameObject trapDoor;
+	private GameObject player1;
+	private GameObject trapDoorCopy;
+	private bool triggered = false;
+
+	public int time = 0;
+	public int delay = 60;
+	private Vector3 startPos;
+
+
+	public Vector3 sendPos = new Vector3(0,0,0);
+
+	public int waitTime = 1000;
+	// Use this for initialization
+	void Start () {
+		//trapDoorCopy = Instantiate<GameObject> (trapDoor);
+		startPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(triggered)
+		{
+			time++;
+			if (time>= delay) 
+			{
+				saveObject();
+				time = 0;
+				triggered = false;
+			}
+		}
+	}
+
+	//This script works for all players going on the trap door.
+	//Could be a new way to kill a player, have the floor come out from under
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player") {
+
+			Debug.Log ("Player Entered TrapDoor Area");
+			triggered = true;
+
+		}
+	}
+
+
+	//attempting to respawn another trap door after the first one gets destroyed
+	void saveObject()
+	{
+		transform.position = sendPos;
+	}
+
+	void loadObject()
+	{
+		transform.position = startPos;
+	}
+
+
+
+}
---
+++ Screen_Cheats/Assets/pickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour {
+    public Vector3 storage, location;
+    public int respawn;
+    public int delay;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(respawn==0)
+        {
+            transform.position = location;
+        }
+        if(respawn>0)
+        {
+            respawn--;
+        }
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Debug.Log(col.gameObject.name);
+            transform.position = storage;
+            respawn = delay;
+        }
+    }
+}
